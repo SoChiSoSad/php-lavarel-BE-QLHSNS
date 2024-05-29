dc0d5065a441585350f54ea0d86c0fdd2d0b6f47
tienthanh11 <dotienthanh1121@gmail.com>
2024-05-30 00:48:12 +0700

    add list personnel

--- hrm-app/routes/api.php
+++ hrm-app/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PersonnelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('users')->group(function () {
+        Route::get('/', [PersonnelController::class, 'index'])->middleware('permission:users.list'); 
+    });
 });
